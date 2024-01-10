f5b14766d8b658ba7855d36b88963717897b414a
Jonathan47G <guestin.jonathan@gmail.com>
2024-01-10 23:49:15 +0100

    modification header, avec un variable pour les classeName

--- quercy-anim/src/conteneurs/Headers.tsx
+++ quercy-anim/src/conteneurs/Headers.tsx
@@ -1,20 +1,45 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import logo from "../../public/images/Logo_Quercy_AnimRedim.png";
 import Link from "next/link";
 
 export default function Header() {
-	
-  	return (
-		<header className="flex justify-around h-[80px] items-center w-full bg-iris-Background-1 border-b-4 border-[#444039]">
+	const lienProps = {
+		lienClassName:
+			"hover:decoration-gold-Primary-text hover:underline decoration-2 underline-offset-8 m-auto pl-7 h-[70px] flex items-center",
+	};
+	const boxLien = {
+		boxClassName:
+			"hover:border-iris-Border-ring-1 pl-3 pr-3 p-2 rounded hover:bg-gold-Interactive-1",
+	};
+	return (
+		<header className="flex justify-between p-5 h-[80px] items-center w-full bg-iris-Background-1 border-b-4 border-[#444039]">
 			<Image src={logo} height={50} alt="logo Quercy Anim" />
-            <nav>
-				<ul className="grid grid-cols-4 gap-5 text-lg text-white font-extrabold divide-x-2 divide-[#444039] *:m-auto *:pl-7 *:h-[70px] *:flex *:items-center ">
-					<li className="hover:decoration-[#696256] hover:underline decoration-4 underline-offset-8"><Link href="/#Accueil" >Accueil</Link></li>
-					<li className="hover:decoration-[#696256] hover:underline decoration-4 underline-offset-8"><Link href="/#Event">Événement</Link></li>
-					<li className="hover:decoration-[#696256] hover:underline decoration-4 underline-offset-8"><Link href="/#Agenda">Agenda</Link></li>
-					<li className="hover:decoration-[#696256] hover:underline decoration-4 underline-offset-8"><Link href="/#A-propos">A propos</Link></li>
+			<nav>
+				<ul className="grid grid-cols-4 gap-5 text-lg text-white font-extrabold divide-x-2 divide-[#444039]">
+					<li className={lienProps.lienClassName}>
+						<Link href="/#Accueil" className={boxLien.boxClassName}>
+							Accueil
+						</Link>
+					</li>
+					<li className={lienProps.lienClassName}>
+						<Link href="/#Event" className={boxLien.boxClassName}>
+							Événement
+						</Link>
+					</li>
+					<li className={lienProps.lienClassName}>
+						<Link href="/#Agenda" className={boxLien.boxClassName}>
+							{" "}
+							Agenda
+						</Link>
+					</li>
+					<li className={lienProps.lienClassName}>
+						<Link href="/#A-propos" className={boxLien.boxClassName}>
+							{" "}
+							A propos
+						</Link>
+					</li>
 				</ul>
 			</nav>
 		</header>
