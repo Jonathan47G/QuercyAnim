c8842f9d3827fc7dc958fcc4aa3a2789830b9d07
Jonathan47G <guestin.jonathan@gmail.com>
2024-01-10 23:49:51 +0100

    Ajout section staff

---
+++ quercy-anim/src/conteneurs/Staff.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Card from "@/elements/Card";
+import Image from "next/image";
+
+export default function Staff() {
+	return (
+		<section>
+			<h2><strong>Notre équipe</strong></h2>
+			<p></p>
+		</section>
+	);
+}
