e32e1cd3646270a3d3322d13332684334ffb8334
Jonathan47G <guestin.jonathan@gmail.com>
2024-01-21 00:29:55 +0100

    Modification taille image, couleur de section et du padding du bouton, ainsi que la forme

--- quercy-anim/src/conteneurs/Retour.tsx
+++ quercy-anim/src/conteneurs/Retour.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function RetourClient() {
 	return (
-		<section className="flex flex-col justify-center items-center bg-gold-Background-1">
-			<h2 className=" mb-[50px] decoration-[#696256] decoration-2 underline underline-offset-[9px] mt-[100px] text-white p-10 font-extrabold ">
+		<section className="flex flex-col justify-center items-center bg-iris-Background-1 pb-[50px]">
+			<h2 className=" mb-[50px] decoration-[#696256] decoration-2 underline underline-offset-[9px] text-white p-10 font-extrabold ">
 				Retour client
 			</h2>
 			<div
@@ -14,14 +14,14 @@ export default function RetourClient() {
 			<Link
 				className="flex justify-center items-center gap-10 mt-[50px] "
 				href="https://www.mariages.net/musique-mariage/quercy-anim--e248285">
-				<p className="font-semibold p-5 rounded-xl transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 text-iris-Primary-text bg-gradient-to-r from-iris-Interactive-1 via-blue-500 to-iris-Interactive-1   hover:via-iris-Interactive-2 hover:to-blue-500">
-					Ici pour pouvoir réserver{" "}
+				<p className="font-semibold p-3 transition duration-300 ease-in-out text-gold-Primary-text bg-gold-Solid-1 border-2 border-gold-Border-sep-1 hover:bg-gold-Solid-2 hover:border-gold-Border-sep-2 hover:-translate-y-1 hover:scale-105 ">
+					Ici pour pouvoir réserver
 				</p>
 				<Image
 					src={badgeMariage}
 					alt="badge mariage.net"
-					width={100}
-					height={100}
+					width={80}
+					height={80}
 				/>
 			</Link>
 		</section>
