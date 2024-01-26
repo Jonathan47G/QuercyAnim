07e2074133ed5362b3d7fb1856bef8f60796c60f
Jonathan47G <guestin.jonathan@gmail.com>
2024-01-26 23:39:18 +0100

    modification lien, seo ect

--- quercy-anim/src/app/layout.tsx
+++ quercy-anim/src/app/layout.tsx
@@ -9,29 +9,34 @@ const playfair = Playfair({ subsets: ["latin"], variable: "--font-playfair" });
 const josefin = Josefin_Sans({
 	subsets: ["latin"],
 	weight: "400",
-  variable: "--font-josefin"
+	variable: "--font-josefin",
 });
 
 export const metadata: Metadata = {
 	title: "Quercy Anim' - Animation",
-	description: "DJ, Discomobile, animation, fête, mariage, anniversaire, événement, musique",
-	authors: {name:"Guestin"} ,
-	keywords: "DJ, Discomobile, village, animation, fête, mariage, anniversaire, événement, musique, entreprise, lumière, animation, animateur",
-	
-	
+	description:
+		"DJ, Discomobile, animation, fête, mariage, anniversaire, événement, musique",
+	authors: { name: "Guestin" },
+	keywords:
+		"DJ, Discomobile, village, animation, fête, mariage, anniversaire, événement, musique, entreprise, lumière, animation, animateur",
+
 	openGraph: {
 		type: "website",
 		url: "https://quercy-anim.vercel.app/",
 		title: "Quercy Anim'",
-		description: "DJ Animateur (Discomobile) spécialisé dans l'animation de fêtes de village, mariages, anniversaires, et autres événements. Apporte une ambiance dynamique et festive à toutes les occasions. Contactez-nous pour créer des moments mémorables avec une musique et une animation adaptées à vos besoins.",
+		description:
+			"DJ Animateur (Discomobile) spécialisé dans l'animation de fêtes de village, mariages, anniversaires, et autres événements. Apporte une ambiance dynamique et festive à toutes les occasions. Contactez-nous pour créer des moments mémorables avec une musique et une animation adaptées à vos besoins.",
 		siteName: "Quercy Anim'",
-		images: [{
-		  url: "https://scontent-cdg4-2.xx.fbcdn.net/v/t39.30808-6/406372072_17932014218789260_1701479443704410279_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=4lOHin_9RcUAX9D2M45&_nc_ht=scontent-cdg4-2.xx&oh=00_AfAXMbPWds2earcoX1-z4NDfN5VBvghwYQ9D06Xr6mLT1A&oe=65B749F3",
-		}],
-	  }
+		images: [
+			{
+				url: "https://scontent-cdg4-2.xx.fbcdn.net/v/t39.30808-6/406372072_17932014218789260_1701479443704410279_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=4lOHin_9RcUAX9D2M45&_nc_ht=scontent-cdg4-2.xx&oh=00_AfAXMbPWds2earcoX1-z4NDfN5VBvghwYQ9D06Xr6mLT1A&oe=65B749F3",
+			},
+		],
+	},
 };
 
 export default function RootLayout({
+	
 	children,
 }: {
 	children: React.ReactNode;
@@ -40,14 +45,18 @@ export default function RootLayout({
 		<html
 			className={`max-w-[2000px] m-auto bg-iris-Background-1 ${playfair.variable} ${josefin.variable}`}
 			lang="fr">
-				<head><Script
-          src="https://static.elfsight.com/platform/platform.js"
-          data-use-service-core
-          defer
-        />
-		<script type="application/ld+json" src="/djSchema.json" async />
-		</head>
-			<body className="bg-iris-Background-2"><Header/> {children} <Footer /></body>
+			<head>
+				<Script
+					src="https://static.elfsight.com/platform/platform.js"
+					data-use-service-core
+					defer
+				/>
+				<link rel="canonical" href="https://quercy-anim.vercel.app/" />
+				<script type="application/ld+json" src="/djSchema.json" async />
+			</head>
+			<body className="bg-iris-Background-2">
+				<Header /> {children} <Footer />
+			</body>
 		</html>
 	);
 }
